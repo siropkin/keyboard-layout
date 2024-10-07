75547c2d4a2b81ccf69beb20e096615a0d8db843
Ivan Seredkin <ivan@netography.com>
2024-10-07 15:35:28 -0400

    Code refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,37 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    alias(libs.plugins.jvm)
-    `java-library`
+    id("java")
     id("com.vanniktech.maven.publish") version "0.29.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Use the JUnit 5 integration.
+    testImplementation(libs.junit.jupiter.engine)
+
+    // Use the Kotlin JUnit 5 integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // Add Mockito dependency
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("org.mockito:mockito-core:5.14.1")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("net.java.dev.jna:jna:5.14.0")
+    implementation("net.java.dev.jna:jna-platform:5.14.0")
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 mavenPublishing {
@@ -11,7 +39,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.siropkin",
         artifactId = "keyboard-layout",
-        version = "1.0.2"
+        version = "1.0.3"
     )
 
     // Configure POM metadata for the published artifact
@@ -48,35 +76,6 @@ mavenPublishing {
 
     // Enable GPG signing for all publications
     signAllPublications()
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    // Use the JUnit 5 integration.
-    testImplementation(libs.junit.jupiter.engine)
-
-    // Use the Kotlin JUnit 5 integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    // Add Mockito dependency
-    testImplementation("org.mockito:mockito-inline:5.2.0")
-    testImplementation("org.mockito:mockito-core:5.14.1")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    implementation("net.java.dev.jna:jna:5.14.0")
-    implementation("net.java.dev.jna:jna-platform:5.14.0")
-}
-
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
 }
 
 tasks.named<Test>("test") {
